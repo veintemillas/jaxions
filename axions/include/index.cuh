4e33625dbe7e6e954002d5e45ff13974a7e373a1
alex <alex@d380d844-bb12-11dd-97b7-c357fe0bd3ad>
2016-10-19 22:45:45 +0000

    - Se usa uint en las funciones de indice sde la gpu - Se soluciona un conflicto de cabeceras

--- axions/include/index.cuh
+++ axions/include/index.cuh
@@ -2,16 +2,16 @@
 
 namespace indexHelper
 {
-	static __host__ __device__ inline void idx2Vec(int idx, int x[3], const int Lx)
+	static __host__ __device__ inline void idx2Vec(uint idx, uint x[3], const uint Lx)
 	{
-		int tmp = idx/Lx;
+		uint tmp = idx/Lx;
 
 		x[2] = tmp/Lx;
 		x[1] = tmp - x[2]*Lx;
 		x[0] = idx - tmp*Lx;
 	}
 
-	static __host__ __device__ inline int vec2Idx(int x[3], const int Lx)
+	static __host__ __device__ inline uint vec2Idx(uint x[3], const uint Lx)
 	{
 		return (x[0] + Lx*(x[1] + Lx*x[2]));
 	}
