a04113303607dd2a23a33797f363f13618e14eb3
alex <alex@d380d844-bb12-11dd-97b7-c357fe0bd3ad>
2016-10-26 20:07:01 +0000

    - Se incluyen las cabeceras de la energia

---
+++ axions/include/energy.h
@@ -0,0 +1,4 @@
+#include "scalarField.h"
+#include "flopCounter.h"
+
+void	energy	(Scalar *field, const double LL, const double nQcd, const double delta, DeviceType dev, void *eRes, FlopCounter *fCount);
