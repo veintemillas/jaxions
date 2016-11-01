53fa7ca827f44c0b17636a670d457e662713a84a
alex <alex@d380d844-bb12-11dd-97b7-c357fe0bd3ad>
2016-11-01 05:47:02 +0000

    -Se termina el codigo de deteccion de cuerdas

---
+++ axions/include/string.h
@@ -0,0 +1,4 @@
+#include "scalarField.h"
+#include "flopCounter.h"
+
+void	string	(Scalar *field, DeviceType dev, void *string, FlopCounter *fCount);
---
+++ axions/lib/string.cpp
@@ -0,0 +1,109 @@
+#include <cstdio>
+#include <cstdlib>
+#include "scalarField.h"
+#include "enum-field.h"
+
+#include "stringXeon.h"
+
+#ifdef	USE_GPU
+	#include <cuda.h>
+	#include <cuda_runtime.h>
+	#include <cuda_device_runtime_api.h>
+	#include "stringGpu.h"
+#endif
+
+#include "flopCounter.h"
+#include "memAlloc.h"
+
+#include <mpi.h>
+
+class	String
+{
+	private:
+
+	const size_t Lx, V, S;
+
+	FieldPrecision precision;
+
+	void    *string;
+	Scalar	*axionField;
+
+	public:
+
+		 String(Scalar *field, void *str);
+		~String() {};
+
+	void	runCpu	();
+	void	runGpu	();
+	void	runXeon	();
+};
+
+	String::String(Scalar *field, void *str) : axionField(field), Lx(field->Length()), V(field->Size()), S(field->Surf()), precision(field->Precision()), string(str)
+{
+}
+
+void	String::runGpu	()
+{
+#ifdef	USE_GPU
+/*
+	const uint uLx = Lx, uLz = Lz, uS = S, uV = V;
+	const uint ext = uV + uS;
+	double *z = axionField->zV();
+
+        energyGpu(axionField->mGpu(), axionField->vGpu(), z, delta2, LL, nQcd, uLx, uLz, 2*uS, uV, precision, ((cudaStream_t *)axionField->Streams())[0]);
+	axionField->exchangeGhosts(FIELD_M);
+        energyGpu(axionField->mGpu(), axionField->vGpu(), z, delta2, LL, nQcd, uLx, uLz, uS, 2*uS, precision, ((cudaStream_t *)axionField->Streams())[0]);
+        energyGpu(axionField->mGpu(), axionField->vGpu(), z, delta2, LL, nQcd, uLx, uLz, uV, ext, precision, ((cudaStream_t *)axionField->Streams())[0]);
+
+	cudaDeviceSynchronize();	// This is not strictly necessary, but simplifies things a lot
+*/
+#else
+	printf("Gpu support not built");
+	exit(1);
+#endif
+}
+
+void	String::runCpu	()
+{
+	stringCpu(axionField, Lx, V, S, precision, string);
+}
+
+void	String::runXeon	()
+{
+#ifdef	USE_XEON
+	energyXeon(axionField, Lx, V, S, precision, string);
+#else
+	printf("Xeon Phi support not built");
+	exit(1);
+#endif
+}
+
+void	string	(Scalar *field, DeviceType dev, void *string, FlopCounter *fCount)
+{
+	String *eStr = new String(field, string);
+
+	switch (dev)
+	{
+		case DEV_CPU:
+			eStr->runCpu ();
+			break;
+
+		case DEV_GPU:
+			eStr->runGpu ();
+			break;
+
+		case	DEV_XEON:
+			eStr->runXeon();
+			break;
+
+		default:
+			printf ("Not a valid device\n");
+			break;
+	}
+
+	delete	eStr;
+
+//	fCount->addFlops((75.*field->Size() - 10.)*1.e-9, 8.*field->dataSize()*field->Size()*1.e-9);
+
+	return;
+}
