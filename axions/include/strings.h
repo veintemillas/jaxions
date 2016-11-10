3788564228b1be47d9f326b634b7f3cf729761a4
alex <alex@d380d844-bb12-11dd-97b7-c357fe0bd3ad>
2016-11-10 06:31:24 +0000

    - Se arregla la escritura de las cuerdas vectorizadas - Se anade un contador de cuerdas y de quiralidad

--- axions/include/strings.h
+++ axions/include/strings.h
@@ -1,6 +1,6 @@
 #include "scalarField.h"
 #include "flopCounter.h"
 
-void	strings	(Scalar *field, DeviceType dev, void *string, FlopCounter *fCount);
+double	strings	(Scalar *field, DeviceType dev, void *string, FlopCounter *fCount);
 
 void	analyzeStrFolded	(Scalar *axion, const int index);
