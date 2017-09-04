9f6d606a1224c7df41d6b45560a562f8cae07ff7
Javier Redondo <redondo@pcl347a.mppmu.mpg.de>
2017-09-04 18:04:30 +0200

    stupid scripts

---
+++ scripts/craxdir.sh
@@ -0,0 +1,17 @@
+mkdir out/
+mkdir out/m
+cp /Users/jared/jaxions/jaxions/scripts/plotStr.py out/m/plotStr.py
+cp /Users/jared/jaxions/jaxions/scripts/plot2D_new.py out/m/plot2D_new.py
+mkdir out/at
+mkdir out/rho
+mkdir out/dens
+mkdir out/con
+mkdir out/dump
+mkdir out/str
+mkdir out/plots
+mkdir out/plots/at
+mkdir out/plots/rho
+mkdir out/plots/str
+mkdir out/plots/dens
+mkdir out/plots/con
+
