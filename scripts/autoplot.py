424293a2a3dd40c2d80db69985c46381b4c5975a
Javier Redondo <jared@rundlufts.local>
2017-09-09 17:00:50 +0200

    autoplot

--- scripts/autoplot.py
+++ scripts/autoplot.py
@@ -55,13 +55,16 @@ with open('./sample.txt') as f:
     arrayA = np.genfromtxt('./sample.txt',skip_header=l10)
     arrayS = np.genfromtxt('./sample.txt',skip_footer=l5)
 
+axiondata = len(arrayA) >0    
+    
 ztab1 = arrayS[:,0]
 Thtab1 = np.arctan2(arrayS[:,4],arrayS[:,3])
 Rhtab1 = np.sqrt(arrayS[:,3]**2 + arrayS[:,4]**2)/ztab1
 VThtab1 = Thtab1 + (arrayS[:,3]*arrayS[:,6]-arrayS[:,4]*arrayS[:,5])/(ztab1*Rhtab1**2)
-ztab2 = arrayA[:,0]
-Thtab2 = arrayA[:,2]/ztab2
-VThtab2 = arrayA[:,3]    
+if axiondata:
+    ztab2 = arrayA[:,0]
+    Thtab2 = arrayA[:,2]/ztab2
+    VThtab2 = arrayA[:,3]    
     
 # CREATE DIR FOR PICS    
 if not os.path.exists('./pics'):
@@ -70,7 +73,8 @@ if not os.path.exists('./pics'):
 # THETA EVOLUTION
 plt.clf()   
 plt.plot(ztab1,Thtab1)
-plt.plot(ztab2,Thtab2)
+if axiondata:
+    plt.plot(ztab2,Thtab2)
 plt.ylim([-3.15,3.15])
 plt.ylabel(r"\theta")
 plt.xlabel(r'\tau')
@@ -90,7 +94,8 @@ plt.savefig("pics/point_rho.pdf")
 # THETA VELOCITY EVOLUTION
 plt.clf()
 plt.plot(ztab1,VThtab1,linewidth=0.2,marker='.',markersize=0.1)
-plt.plot(ztab2,VThtab2,linewidth=0.2)
+if axiondata:
+    plt.plot(ztab2,VThtab2,linewidth=0.2)
 plt.ylabel(r"\rho/v-1")
 plt.xlabel(r'\tau')
 plt.title(ups)
@@ -158,143 +163,172 @@ print()
 f = h5py.File('./m/'+fileMeas[-1], 'r')
 for item in f.attrs.keys():
     print(item + ":", f.attrs[item])
-
+        
 print('contains ' + str(list(f)))    
 
+an_cont = False
+an_nspec = False
+an_pspec = False
 
-tc = np.reshape(f['bins/cont'],(10000))
-avdens, maxcon, logmaxcon = tc[0:3]
-bino = tc[3:]
-numbins=10000-3
-#binsizecons = numbins/(N3*math.log(10)*(logmaxcon+5))
-#print(avdens, maxcon, logmaxcon , numbins, binsizecons)
-#contab=10**((logmaxcon+5)*(np.arange(numbins)+0.5)/numbins - 5)
-#auxtab=binsizecons*bino/contab
+for item in list(f):
+    if item == 'bins':
+        print('contrast bin posible')
+        an_cont = True
+    if item == 'nSpectrum':
+        print('nSpec bin posible')
+        an_nspec = True    
+    if item == 'pSpectrum':
+        print('pSpec bin posible')
+        an_pspec = True        
 
-# PROCESS IT
-# BINS SELECTED BY  bin = int[(5+log10[d])*numbins/(logmaxcon+5)]
-# DISCARD SMALL BINS AT BEGGINNING
-# JOIN BINS AT THE END
-i0 = 0
-while bino[i0] < 1.99:
-        i0 = i0 + 1
-print(i0)
+if an_cont:    
+    tc = np.reshape(f['bins/cont'],(10000))
+    avdens, maxcon, logmaxcon = tc[0:3]
+    bino = tc[3:]
+    numbins=10000-3
+    #binsizecons = numbins/(N3*math.log(10)*(logmaxcon+5))
+    #print(avdens, maxcon, logmaxcon , numbins, binsizecons)
+    #contab=10**((logmaxcon+5)*(np.arange(numbins)+0.5)/numbins - 5)
+    #auxtab=binsizecons*bino/contab
 
-bino = bino[i0:]
-#contab = contab[i0:]
+    # PROCESS IT
+    # BINS SELECTED BY  bin = int[(5+log10[d])*numbins/(logmaxcon+5)]
+    # DISCARD SMALL BINS AT BEGGINNING
+    # JOIN BINS AT THE END
+    i0 = 0
+    while bino[i0] < 1.99:
+            i0 = i0 + 1
+    print(i0)
 
-sum = 0
-parsum = 0
-nsubbin = 0
-minimum = 10
-lista=[]
-for bin in range(0,len(bino)):
-    parsum += bino[bin]
-    nsubbin += 1
-    if nsubbin ==1:
-            inbin = bin
-    if parsum < 10:
-        sum += 1
-    else:
-        enbin = bin    
-        # rebin and reweight
-        # bin corresponds to i0 + Dbin to contrast 10**((logmaxcon+5)*(<bin,bin+1>)/numbins - 5)
-        # so one can just compute initial and final and divide
-        ## binsizecons = numbins/(N3*math.log(10)*(logmaxcon+5))
-        ## contab=10**((logmaxcon+5)*(np.arange(numbins)+0.5)/numbins - 5)
-        ## auxtab=binsizecons*bino/contab
-        low = 10**((logmaxcon+5)*(i0+inbin)/numbins - 5)
-        med = 10**((logmaxcon+5)*(i0+(inbin+enbin+1)/2)/numbins - 5)
-        sup = 10**((logmaxcon+5)*(i0+enbin+1)/numbins - 5)
-        lista.append([med,parsum/(sup-low)])
-        
-        parsum = 0
-        nsubbin = 0
-        
-lis = np.array(lista)
+    bino = bino[i0:]
+    #contab = contab[i0:]
 
-plt.clf()
-#plt.loglog(contab,auxtab,       c='b',linewidth=0.3,marker='.',markersize=0.1)
-plt.loglog(lis[:,0],lis[:,1]/N3,c='green',linewidth=0.6,marker='.',markersize=0.1)
-plt.ylabel(r'dP/d\delta')
-plt.xlabel(r'\delta')
-plt.title(ups)
-plt.savefig("pics/contrastbin.pdf")
-#plt.show()
+    sum = 0
+    parsum = 0
+    nsubbin = 0
+    minimum = 10
+    lista=[]
+    for bin in range(0,len(bino)):
+        parsum += bino[bin]
+        nsubbin += 1
+        if nsubbin ==1:
+                inbin = bin
+        if parsum < 10:
+            sum += 1
+        else:
+            enbin = bin    
+            # rebin and reweight
+            # bin corresponds to i0 + Dbin to contrast 10**((logmaxcon+5)*(<bin,bin+1>)/numbins - 5)
+            # so one can just compute initial and final and divide
+            ## binsizecons = numbins/(N3*math.log(10)*(logmaxcon+5))
+            ## contab=10**((logmaxcon+5)*(np.arange(numbins)+0.5)/numbins - 5)
+            ## auxtab=binsizecons*bino/contab
+            low = 10**((logmaxcon+5)*(i0+inbin)/numbins - 5)
+            med = 10**((logmaxcon+5)*(i0+(inbin+enbin+1)/2)/numbins - 5)
+            sup = 10**((logmaxcon+5)*(i0+enbin+1)/numbins - 5)
+            lista.append([med,parsum/(sup-low)])
+
+            parsum = 0
+            nsubbin = 0
+
+    lis = np.array(lista)
+
+    plt.clf()
+    #plt.loglog(contab,auxtab,       c='b',linewidth=0.3,marker='.',markersize=0.1)
+    plt.loglog(lis[:,0],lis[:,1]/N3,c='green',linewidth=0.6,marker='.',markersize=0.1)
+    plt.ylabel(r'dP/d\delta')
+    plt.xlabel(r'\delta')
+    plt.title(ups)
+    plt.savefig("pics/contrastbin.pdf")
+    #plt.show()
 
 
 # NUMBER SPECTRUM
-n2=int(sizeN/2)
+if an_nspec:
+    from scipy.optimize import curve_fit    
+    n2=int(sizeN/2)
 
-from scipy.optimize import curve_fit
+    f = h5py.File('./m/'+fileMeas[-1], 'r')
+    time = f.attrs[u'z']
+    powmax = f['nSpectrum/sK'].size
+    ktab = (0.5+np.arange(powmax))*2*math.pi/sizeL
 
-f = h5py.File('./m/'+fileMeas[-1], 'r')
-time = f.attrs[u'z']
-powmax = f['nSpectrum/sK'].size
-ktab = (0.5+np.arange(powmax))*2*math.pi/sizeL
+    larvaK = np.reshape(f['nSpectrum/sK'],(powmax))
+    larvaG = np.reshape(f['nSpectrum/sG'],(powmax))
+    larvaV = np.reshape(f['nSpectrum/sV'],(powmax))
 
-larvaK = np.reshape(f['nSpectrum/sK'],(powmax))
-larvaG = np.reshape(f['nSpectrum/sG'],(powmax))
-larvaV = np.reshape(f['nSpectrum/sV'],(powmax))
+    def funi(x,a,b):
+        return a + b*x
 
-def funi(x,a,b):
-    return a + b*x
+    from math import exp, log10, fabs, atan, log, atan2
 
-from math import exp, log10, fabs, atan, log, atan2
+    def volu( rR ):
+        if rR <= 1.0:
+            return (4*math.pi/3)*rR**3
 
-def volu( rR ):
-    if rR <= 1.0:
-        return (4*math.pi/3)*rR**3
+        elif 1.0 < rR <= math.sqrt(2.):
+            return (2*math.pi/3)*(9*rR**2-4*rR**3-3)
 
-    elif 1.0 < rR <= math.sqrt(2.):
-        return (2*math.pi/3)*(9*rR**2-4*rR**3-3)
+        elif math.sqrt(2.) < rR < math.sqrt(3.):
+            a2 = rR**2-2
+            a = math.sqrt(a2)
+            b = 8*a - 4*(3*rR**2 -1)*(atan(a)-atan(1/a))
+            return b - (8/3)*(rR**3)*atan2(a*(6*rR + 4*rR**3 -2*rR**5),6*rR**4-2-12*rR**2) 
 
-    elif math.sqrt(2.) < rR < math.sqrt(3.):
-        a2 = rR**2-2
-        a = math.sqrt(a2)
-        b = 8*a - 4*(3*rR**2 -1)*(atan(a)-atan(1/a))
-        return b - (8/3)*(rR**3)*atan2(a*(6*rR + 4*rR**3 -2*rR**5),6*rR**4-2-12*rR**2) 
- 
-    elif  math.sqrt(3) < rR:
-        return 8.
-    
-vecvolu=np.vectorize(volu)  
+        elif  math.sqrt(3) < rR:
+            return 8.
 
-foca = np.arange(0,powmax)/n2
-foca2 = np.arange(1,powmax+1)/n2
-nmodes2 = (n2**3)*(vecvolu(foca2)-vecvolu(foca))
+    vecvolu=np.vectorize(volu)  
 
-#OCUPATION NUMBER is more interesting D.9 of notes
-nK = larvaK/nmodes2
-nG = larvaG/nmodes2
-nV = larvaV/nmodes2
+    foca = np.arange(0,powmax)/n2
+    foca2 = np.arange(1,powmax+1)/n2
+    nmodes2 = (n2**3)*(vecvolu(foca2)-vecvolu(foca))
 
-lima = int(sizeN/2)
-popt, pcov = curve_fit(funi, np.log10(ktab[:lima]),np.log10(nK+nV+nG)[:lima])
-popt[0]
-koni = 10**popt[0]
-indi = popt[1]
-print(koni, indi)
-'%.1f k^%.1f'%(koni,indi)
+    #OCUPATION NUMBER is more interesting D.9 of notes
+    nK = larvaK/nmodes2
+    nG = larvaG/nmodes2
+    nV = larvaV/nmodes2
 
-plt.clf()
-plt.loglog(ktab[:lima],koni*ktab[:lima]**indi,c='gray',linewidth=1,label=r'$%.1f\, k^{%.1f}$' % (koni,indi))
-plt.loglog(ktab[:-2],nK[:-2],c='r',linewidth=0.6,marker='.',markersize=0.1,label='K')
-plt.loglog(ktab[:-2],nG[:-2],c='b',linewidth=0.6,marker='.',markersize=0.1,label='G')
-plt.loglog(ktab[:-2],nV[:-2],c='k',linewidth=0.6,marker='.',markersize=0.1,label='V')
-plt.loglog(ktab[:-2],(nK+nV+nG)[:-2],c='k',linewidth=1,label='K+V+G')
-plt.title(ups)
-#plt.ylim([0.00000001,100])
-plt.ylabel(r'$n_k$')
-plt.xlabel(r'comoving {$k [1/R_1 H_1]$}')
-plt.legend(loc='lower left',title=r'$\tau$={%.1f}'%(time))
-plt.savefig("pics/occnumber.pdf")
-#plt.show()
+    lima = int(sizeN/2)
+    popt, pcov = curve_fit(funi, np.log10(ktab[:lima]),np.log10(nK+nV+nG)[:lima])
+    popt[0]
+    koni = 10**popt[0]
+    indi = popt[1]
+    print(koni, indi)
+    '%.1f k^%.1f'%(koni,indi)
 
+    plt.clf()
+    plt.loglog(ktab[:lima],koni*ktab[:lima]**indi,c='gray',linewidth=1,label=r'$%.1f\, k^{%.1f}$' % (koni,indi))
+    plt.loglog(ktab[:-2],nK[:-2],c='r',linewidth=0.6,marker='.',markersize=0.1,label='K')
+    plt.loglog(ktab[:-2],nG[:-2],c='b',linewidth=0.6,marker='.',markersize=0.1,label='G')
+    plt.loglog(ktab[:-2],nV[:-2],c='k',linewidth=0.6,marker='.',markersize=0.1,label='V')
+    plt.loglog(ktab[:-2],(nK+nV+nG)[:-2],c='k',linewidth=1,label='K+V+G')
+    plt.title(ups)
+    #plt.ylim([0.00000001,100])
+    plt.ylabel(r'$n_k$')
+    plt.xlabel(r'comoving {$k [1/R_1 H_1]$}')
+    plt.legend(loc='lower left',title=r'$\tau$={%.1f}'%(time))
+    plt.savefig("pics/occnumber.pdf")
+    #plt.show()
 
+# POWER SPECTRUM
+if an_pspec:
+    powmax2 = f['pSpectrum/sP'].size
+    ktab2 = (0.5+np.arange(powmax2))*2*math.pi/sizeL
+    f['pSpectrum/sP'] , powmax2 , (sizeN/2)*math.sqrt(3)
+    larvaP = np.reshape(f['pSpectrum/sP'],(powmax2))
+    av = larvaP/nmodes2
 
-
-
+    plt.clf()
+    plt.loglog(ktab2,(ktab2**3)*av/(math.pi**2),label=r'$\tau$={%.1f}'%(time))
+    plt.loglog(ktab2,np.ones(powmax2))
+    plt.title(ups)
+    #plt.ylim([0.00000001,100])
+    plt.ylabel(r'$\Delta^2_k$')
+    plt.xlabel(r'comoving {$k [1/R_1 H_1]$}')
+    plt.legend(loc='lower left',title=r'$\tau$={%.1f}'%(time))
+    plt.savefig("pics/powerspectrum.pdf")
+    #plt.show()
 
 
 
