08372c9aa2df9097e6b6184716c2f2b20b8a8673
Alejandro Vaquero <serianam@gmail.com>
2017-10-25 22:35:10 -0600

    Removed unused files

--- include/scalar/mendThetaGpu.h
+++
@@ -1,8 +0,0 @@
-#ifndef	_MEND_GPU_
-	#define	_MEND_GPU_
-
-	#include "scalar/scalarField.h"
-	#include "enum-field.h"
-
-	uint	mendThetaGpu	(void * __restrict__ m, void * __restrict__ v, const double z, const uint Lx, const uint Vo, const uint Vf, FieldPrecision precision, cudaStream_t &stream);
-#endif
