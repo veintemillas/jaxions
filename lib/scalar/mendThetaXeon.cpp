7ace87932075bbfe998b8e3ff220eed44cb8df33
Alejandro Vaquero <serianam@gmail.com>
2017-11-06 13:06:00 -0600

    Fixed hanging problem with mendTheta

--- lib/scalar/mendThetaXeon.cpp
+++ lib/scalar/mendThetaXeon.cpp
@@ -38,12 +38,15 @@ inline  size_t	mendThetaKernelXeon(void * __restrict__ m_, void * __restrict__ v
 #ifdef  __AVX512F__
 	#define _MData_ __m512d
 	#define step 8
+	#define shf  3
 #elif   defined(__AVX__)
 	#define _MData_ __m256d
 	#define step 4
+	#define shf  2
 #else
 	#define _MData_ __m128d
 	#define step 2
+	#define shf  1
 #endif
 
 		double * __restrict__ m = (double * __restrict__) __builtin_assume_aligned (m_, Align);
@@ -51,16 +54,9 @@ inline  size_t	mendThetaKernelXeon(void * __restrict__ m_, void * __restrict__ v
 
 		const double zP = M_PI*z;
 
-#ifdef  __AVX512F__
-		const size_t XC = (Lx<<3);
-		const size_t YC = (Lx>>3);
-#elif   defined(__AVX__)
-		const size_t XC = (Lx<<2);
-		const size_t YC = (Lx>>2);
-#else
-		const size_t XC = (Lx<<1);
-		const size_t YC = (Lx>>1);
-#endif
+		const size_t XC = (Lx<<shf);
+		const size_t YC = (Lx>>shf);
+
 		const _MData_ pVec  = opCode(set1_pd, +zP);
 		const _MData_ mVec  = opCode(set1_pd, -zP);
 		const _MData_ vVec  = opCode(set1_pd, 2.*M_PI);
@@ -99,11 +95,8 @@ inline  size_t	mendThetaKernelXeon(void * __restrict__ m_, void * __restrict__ v
 							mPx = opCode(mask_add_pd, mPx, mMask, mPx, cVec);
 							vPx = opCode(mask_add_pd, vPx, mMask, vPx, vVec);
 
-							int i=1;
-							for (int k=0; k<step; k++) {
+							for (int k=0,i=1; k<step; k++,i<<=1)
 								count += (mask & i) >> k;
-								i<<=1;
-							}
 
 							mDf  = opCode(sub_pd, mPx, mel);
 
@@ -112,6 +105,8 @@ inline  size_t	mendThetaKernelXeon(void * __restrict__ m_, void * __restrict__ v
 							mask = pMask | mMask;
 						}
 #else
+						size_t mask = 0;
+						size_t msk[step];
 	#ifdef	__AVX__
 						mDp = opCode(cmp_pd, mDf, pVec, _CMP_GE_OQ);
 						mDm = opCode(cmp_pd, mDf, mVec, _CMP_LT_OQ);
@@ -120,10 +115,10 @@ inline  size_t	mendThetaKernelXeon(void * __restrict__ m_, void * __restrict__ v
 						mDm = opCode(cmplt_pd, mDf, mVec);
 	#endif
 						mDc = opCode(or_pd, mDp, mDm);
-						size_t mask = 0;
+						opCode(store_pd, static_cast<double*>(static_cast<void*>(msk)), mDc);
 
 						for (int k=0; k<step; k++)
-							mask += reinterpret_cast<size_t&>(mDc[k]) & 1;
+							mask += msk[k] & 1;
 
 						while	(mask != 0) {
 							mPx = opCode(sub_pd, mPx, opCode(and_pd, mDp, cVec));
@@ -143,10 +138,11 @@ inline  size_t	mendThetaKernelXeon(void * __restrict__ m_, void * __restrict__ v
 	#endif
 							mDc = opCode(or_pd, mDp, mDm);
 
-							mask = 0;
+							opCode(store_pd, static_cast<double*>(static_cast<void*>(msk)), mDc);
 
+							mask = 0;
 							for (int k=0; k<step; k++)
-								mask += reinterpret_cast<size_t&>(mDc[k]) & 1;
+								mask += msk[k] & 1;
 						}
 #endif	// AVX and SSE4.1
 
@@ -161,33 +157,31 @@ inline  size_t	mendThetaKernelXeon(void * __restrict__ m_, void * __restrict__ v
 
 #undef  _MData_
 #undef  step
+#undef  shf
 	}
 	else if (precision == FIELD_SINGLE)
 	{
 #ifdef  __AVX512F__
 	#define _MData_ __m512
 	#define step 16
+	#define shf  4
 #elif   defined(__AVX__)
 	#define _MData_ __m256
 	#define step 8
+	#define shf  3
 #else
 	#define _MData_ __m128
 	#define step 4
+	#define shf  2
 #endif
 		float * __restrict__ m = (float * __restrict__) __builtin_assume_aligned (m_, Align);
 		float * __restrict__ v = (float * __restrict__) __builtin_assume_aligned (v_, Align);
 
 		const float zP = M_PI*z;
-#ifdef  __AVX512F__
-		const size_t XC = (Lx<<4);
-		const size_t YC = (Lx>>4);
-#elif   defined(__AVX__)
-		const size_t XC = (Lx<<3);
-		const size_t YC = (Lx>>3);
-#else
-		const size_t XC = (Lx<<2);
-		const size_t YC = (Lx>>2);
-#endif
+
+		const size_t XC = (Lx<<shf);
+		const size_t YC = (Lx>>shf);
+
 		const _MData_ pVec  = opCode(set1_ps, +zP);
 		const _MData_ mVec  = opCode(set1_ps, -zP);
 		const _MData_ vVec  = opCode(set1_ps, 2.*M_PI);
@@ -200,7 +194,7 @@ inline  size_t	mendThetaKernelXeon(void * __restrict__ m_, void * __restrict__ v
 
 			/*	Collapse loops so OpenMP handles the plane YZ	*/
 			#pragma omp for collapse(2) schedule(static)
-			for (size_t zSl = 1; zSl <= Lz; zSl++)
+			for (size_t zSl = 1; zSl <= Lz; zSl++) {
 				for (size_t yLn = 0; yLn < YC; yLn++) {
 					/*	Bulk loop in X	*/
 					for(size_t xPt = 0; xPt < XC-step; xPt += step) {
@@ -225,11 +219,8 @@ inline  size_t	mendThetaKernelXeon(void * __restrict__ m_, void * __restrict__ v
 							mPx = opCode(mask_add_ps, mPx, mMask, mPx, cVec);
 							vPx = opCode(mask_add_ps, vPx, mMask, vPx, vVec);
 
-							int i=1;
-							for (int k=0; k<step; k++) {
+							for (int k=0,i=1; k<step; k++,i<<=1)
 								count += (mask & i) >> k;
-								i<<=1;
-							}
 
 							mDf  = opCode(sub_ps, mPx, mel);
 
@@ -238,6 +229,8 @@ inline  size_t	mendThetaKernelXeon(void * __restrict__ m_, void * __restrict__ v
 							mask = pMask | mMask;
 						}
 #else
+						int mask = 0;
+						int msk[step];
 	#ifdef	__AVX__
 						mDp = opCode(cmp_ps, mDf, pVec, _CMP_GE_OQ);
 						mDm = opCode(cmp_ps, mDf, mVec, _CMP_LT_OQ);
@@ -246,10 +239,10 @@ inline  size_t	mendThetaKernelXeon(void * __restrict__ m_, void * __restrict__ v
 						mDm = opCode(cmplt_ps, mDf, mVec);
 	#endif
 						mDc = opCode(or_ps, mDp, mDm);
-						size_t mask = 0;
+						opCode(store_ps, static_cast<float*>(static_cast<void*>(msk)), mDc);
 
 						for (int k=0; k<step; k++)
-							mask += reinterpret_cast<int&>(mDc[k]) & 1;
+							mask += msk[k] & 1;
 
 						while	(mask != 0) {
 							mPx = opCode(sub_ps, mPx, opCode(and_ps, mDp, cVec));
@@ -268,20 +261,22 @@ inline  size_t	mendThetaKernelXeon(void * __restrict__ m_, void * __restrict__ v
 							mDm = opCode(cmplt_ps, mDf, mVec);
 	#endif
 							mDc = opCode(or_ps, mDp, mDm);
+							opCode(store_ps, static_cast<float*>(static_cast<void*>(msk)), mDc);
 
 							mask = 0;
-
 							for (int k=0; k<step; k++)
-								mask += reinterpret_cast<int&>(mDc[k]) & 1;
+								mask += msk[k] & 1;
 						}
 #endif
 						opCode(store_ps, &m[idxPx], mPx);
 						opCode(store_ps, &v[idxVx], vPx);
 					}
 				}
+			}
 		}
 #undef  _MData_
 #undef  step
+#undef  shf
 	}
 
 	return	count;
@@ -305,7 +300,7 @@ inline  size_t	mendThetaSlice(Float * __restrict__ m, Float * __restrict__ v, co
 		cnt >>= 1;
 		shf++;
 	}
-		
+
 	const size_t XC = (Lx<<shf);
 	const size_t YC = (Lx>>shf);
 
@@ -407,8 +402,7 @@ inline  size_t	mendThetaLine(Float * __restrict__ m, Float * __restrict__ v, con
 			}
 
 			if (rank == cRank) {
-				MPI_Status status;
-				MPI_Recv(m,         sizeof(Float), MPI_CHAR, bckNeig, cRank, MPI_COMM_WORLD, &status);
+				MPI_Recv(m,         sizeof(Float), MPI_CHAR, bckNeig, cRank, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 			}
 		}
 
