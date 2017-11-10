0d66a928f6a5b57c313e013034ed8d6691813127
Javier Redondo <javier.redondo.martin@gmail.com>
2017-11-10 13:01:56 +0100

    fixed bug

--- scripts/h5cpBNS.sh
+++ scripts/h5cpBNS.sh
@@ -1,5 +1,6 @@
 echo " " h5cpBNS Copies bins/ nSpectrum/ pSpectrum/ from 1st to 2nd input
 echo " " from axion.m."$1" to "$2"
+
 read -r -p ' Are you bloody sure?? [y/n]:' CONT
 if [[ "$CONT" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
  echo '   your call!'
@@ -7,12 +8,14 @@ else
   exit 1
 fi
 
-h5copy -i axion.m."$1" -o axion.m."$2" -s /bins -d /bins
-h5copy -i axion.m."$1" -o axion.m."$2" -s /nSpectrum -d /nSpectrum
-h5copy -i axion.m."$1" -o axion.m."$2" -s /pSpectrum -d /pSpectrum
+
+
+h5copy -i ./axion.m."$1" -o ./axion.m."$2" -s /bins -d /bins
+h5copy -i ./axion.m."$1" -o ./axion.m."$2" -s /nSpectrum -d /nSpectrum
+h5copy -i ./axion.m."$1" -o ./axion.m."$2" -s /pSpectrum -d /pSpectrum
 echo " done!"
 
-echo " have a look at h5ls axion.m."$2"
-h5ls axion.m."$2"
-echo " and inside ... "
-h5ls axion.m."$2"/nSpectrum
+echo " "have a look at h5ls axion.m."$2"
+h5ls ./axion.m."$2"
+echo " "and inside ... nSpectrum
+h5ls ./axion.m."$2"/nSpectrum
