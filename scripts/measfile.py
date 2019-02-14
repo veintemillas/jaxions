e64a364f8af61d549756ab58f5c676b849b3b19a
veintemillas <jredondo@unizar.es>
2019-02-14 15:32:02 +0100

    even better

--- scripts/measfile.py
+++ scripts/measfile.py
@@ -6,45 +6,61 @@ a = pa.mli()
 a.N   = 256
 a.L   = 6.0
 a.msa = 1.0
-a.ctend= 4.0
+a.ctend= 3.5
 
 # Scaling measurements
 a.me  = 0
 a.me_adds("string")
 a.me_adds("energy")
 a.nmt_prt()
-a.addset(7*4,1.0,8.0,0,'logi')
+a.addset(9*4,0.0,9.0,0,'logi')
 
 # Refine close to DM annihilation
 a.me  = 0
 a.me_adds("string")
 a.me_adds("energy")
+a.me_adds("bin theta")
+a.me_adds("bin rho")
+a.me_adds("bin logtheta")
+a.me_adds("bin contrast")
 a.nmt_prt()
-a.addset(1.0*10,1.8,2.8,0,'lin')
+a.addset(20,1.8,2.8,0,'lin')
 
 # A spectrum from time to time
 a.me  = 0
 a.me_adds("string")
+a.me_adds("bin theta")
+a.me_adds("bin rho")
+a.me_adds("bin logtheta")
+a.me_adds("bin contrast")
 a.me_adds("plot2D")
 a.me_adds("NSP_A")
 a.me_adds("PSP_A")
 a.nmt_prt()
-a.addset(6*2,2.0,8.0,0,'logi')
+a.addset(7*2,2.0,9.0,0,'logi')
 
 # Refine spectra during the NR period
 a.me  = 0
 a.me_adds("string")
+a.me_adds("bin theta")
+a.me_adds("bin rho")
+a.me_adds("bin logtheta")
+a.me_adds("bin contrast")
 a.me_adds("plot2D")
 a.me_adds("NSP_A")
 a.me_adds("PSP_A")
 a.nmt_prt()
-a.addset(20,1.5,4.0,0,'lin')
+a.addset(25,1.5,4.0,0,'lin')
 
 # Final dump
 a.me  = 0
-a.me_adds("plot2D")
+#a.me_adds("plot2D")
+a.me_adds("bin theta")
+a.me_adds("bin rho")
+a.me_adds("bin logtheta")
+a.me_adds("bin contrast")
 # a.me_adds("plot3D") # better specify this with --p3D 2 (final) or --p3D 6 (final+wkb)
-a.me_adds("reduced energy map")
+#a.me_adds("reduced energy map")  # better specify this with --redmp --redmpwkb
 a.me_adds("NSP_A")
 a.me_adds("PSP_A")
 a.nmt_prt()
