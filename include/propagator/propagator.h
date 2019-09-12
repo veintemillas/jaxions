af55a72927f735a2e0eecdb161b5cd4390612a15
veintemillas <jredondo@unizar.es>
2019-09-12 03:26:21 +0200

    Header for init propagator with  N for multi-N laplacian

--- include/propagator/propagator.h
+++ include/propagator/propagator.h
@@ -3,7 +3,7 @@
 
 	#include "scalar/scalarField.h"
 
-	void	initPropagator	(PropType pType, Scalar *field, VqcdType pot);
+ 	void	initPropagator	(PropType pType, Scalar *field, VqcdType pot, int Ng=-1);
 	void	propagate	(Scalar *field, const double dz);
 	void	resetPropagator	(Scalar *field);
 	void	tunePropagator	(Scalar *field);
