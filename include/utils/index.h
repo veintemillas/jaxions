c1aee1327e1d2d41fb4c82282abc6fbd32f709ec
veintemillas <jredondo@unizar.es>
2019-10-13 17:16:30 +0200

    Include a function to return neightbours in X and Y

--- include/utils/index.h
+++ include/utils/index.h
@@ -15,4 +15,43 @@ namespace indexXeon
 	{
 		return (x[0] + Lx*(x[1] + Lx*x[2]));
 	}
+
+	static inline void idx2VecNeigh(size_t idx, size_t X[3], size_t O[4], const size_t L)
+	{
+			size_t tmp = idx/L;
+			size_t   S = L*L;
+
+			X[2] = tmp/L;
+			X[1] = tmp - X[2]*L;
+			X[0] = idx - tmp*L;
+
+			// O = iPx, iMx, iPy, iMy
+			if (X[0] == 0) {
+				O[0] = idx + 1; // iPx
+				O[1] = idx + L - 1;
+			} else {
+				if (X[0] == L - 1) {
+					O[0] = idx - L + 1;
+					O[1] = idx - 1;
+				} else {
+					O[0] = idx + 1;
+					O[1] = idx - 1;
+				}
+			}
+			if (X[1] == 0) {
+				O[2] = idx + L;
+				O[3] = idx + S - L;
+			} else {
+				if (X[1] == L - 1) {
+					O[2] = idx - S + L;
+					O[3] = idx - L;
+				} else {
+					O[2] = idx + L;
+					O[3] = idx - L;
+				}
+			}
+
+
+	}
+
 }
