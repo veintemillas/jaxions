eba96c00655f2e3bc05b344156500cd8fa266642
veintemillas <jredondo@unizar.es>
2019-11-10 11:36:44 +0900

    Adapted propagator to n-neighbours; Corrected several bugs

--- lib/scalar/thetaXeon.cpp
+++ lib/scalar/thetaXeon.cpp
@@ -10,17 +10,23 @@ void	toThetaKernelXeon (Scalar *sField, const Float shift)
 {
 	LogMsg(VERB_NORMAL, "[2tX] To theta Xeon ... ");
 
+	// number of ghost zones
+	const size_t NG = sField->getNg();
 	const size_t V  = sField->Size();
 	const size_t S  = sField->Surf();
 	const size_t Lz = sField->Depth();
-	const size_t Go = 2*(V+S);
 
-	Float *mField   = static_cast<Float*>(sField->mStart());
-	Float *vField   = static_cast<Float*>(sField->vCpu());
-	Float *aField1   = static_cast<Float*>(sField->mFrontGhost()) ;
-	Float *aField2   = static_cast<Float*>(sField->mBackGhost()) ;
+	/* Pointers to complex number */
 	complex<Float> *cmField  = static_cast<complex<Float>*>(sField->mStart());
 	complex<Float> *cvField  = static_cast<complex<Float>*>(sField->vCpu());
+
+	/* Pointers to the to-be float array
+		they need to be shifted by the ghost number
+		consistently with the definition in scalarField.h */
+	Float *mField   = static_cast<Float*>(static_cast<void *>(static_cast<char *>(sField->mCpu()) + sField->Precision()*S*NG));
+	Float *vField   = static_cast<Float*>(sField->vCpu());
+	Float *aField1  = static_cast<Float*>(sField->mFrontGhost()) ;
+	Float *aField2  = static_cast<Float*>(sField->mBackGhost()) ;
 
 	const Float z = static_cast<Float>(sField->zV()[0]);
 
@@ -31,10 +37,10 @@ void	toThetaKernelXeon (Scalar *sField, const Float shift)
 		for (size_t lpc = 0; lpc < S; lpc++)
 		{
 			complex<Float> mTmp = cmField[Vo+lpc] - complex<Float>(shift,0.);
-			Float iMod      = z/(mTmp.real()*mTmp.real() + mTmp.imag()*mTmp.imag());
+			Float iMod     = z/(mTmp.real()*mTmp.real() + mTmp.imag()*mTmp.imag());
 			aField1[lpc]   = arg(mTmp);
-			aField2[lpc]   = (cvField[lpc]*conj(mTmp)).imag()*iMod + aField1[lpc];
-			aField1[lpc]    *= z;
+			aField2[lpc]   = (cvField[Vo+lpc]*conj(mTmp)).imag()*iMod + aField1[lpc];
+			aField1[lpc]   *= z;
 		}
 		memcpy (mField + Vo, aField1, sizeof(Float)*S);
 		memcpy (vField + Vo, aField2, sizeof(Float)*S);
