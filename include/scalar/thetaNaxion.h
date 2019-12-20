cee3547e77b338a902bdb7dd701dda59ee70f6c5
Javier Redondo <javier.redondo.martin@gmail.com>
2019-12-20 16:59:10 +0100

    misssing headers

---
+++ include/scalar/thetaNaxion.h
@@ -0,0 +1,7 @@
+#ifndef	_THETA_NAXION_
+	#define	_THETA_NAXION_
+
+	#include "scalar/scalarField.h"
+
+	void	thetaToNaxion	(Scalar *field );
+#endif
---
+++ include/scalar/thetaPaxion.h
@@ -0,0 +1,7 @@
+#ifndef	_THETA_PAXION_
+	#define	_THETA_PAXION_
+
+	#include "scalar/scalarField.h"
+
+	void	thetaToPaxion	(Scalar *field );
+#endif
