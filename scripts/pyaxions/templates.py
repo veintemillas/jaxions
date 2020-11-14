8008304b4e6493457cf144ff6399ebd491907cdd
veintemillas <jredondo@unizar.es>
2020-11-14 18:44:34 +0100

    The output of multiCASECPU can be used in the propagator templates

--- scripts/pyaxions/templates.py
+++ scripts/pyaxions/templates.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 
 
+# prefix='V_'
+# l_QCD=['0','1','2','C','V','L']
+# l_PQ=['1','2']
+# l_ex=['RHO','DRHO','DRHO_RHO','DALL']
+
 prefix='V_'
-l_QCD=['0','1','2','C','V','L']
-l_PQ=['1','2']
+l_QCD=['0','1','C','V','L']
+l_PQ=['1']
 l_ex=['RHO','DRHO','DRHO_RHO','DALL']
+
 
 PR={'PROP_OMELYAN2':'PropOmelyan2', 'PROP_OMELYAN4':'PropOmelyan4',
     'PROP_LEAP':'PropLeap', 'PROP_MLEAP':'PropMLeap', 'PROP_RKN4':'PropRKN4'}
@@ -27,24 +33,24 @@ def multicase(prop):
                 tempCPU(vqcd,PR[prop])
     print('} \n break; \n')
 
-def CASECPU(vqcd,classname):
+def CASECPU(vqcd):
     print('\t CASE(aPT,%s) \\'%vqcd)
 
-def multiCASECPU(prop):
+def multiCASECPU():
     print('case PT1:          \\')
     print('switch (pot) {     \\')
     for q in l_QCD:
         for p in l_PQ:
             vqcd=prefix+'QCD'+q+'_PQ'+p
-            CASECPU(vqcd,PR[prop])
+            CASECPU(vqcd)
             for e in l_ex:
                 vqcd=prefix+'QCD'+q+'_PQ'+p+'_'+e
-                CASECPU(vqcd,PR[prop])
+                CASECPU(vqcd)
     print('} \\\n break; \n')
 
 # run this to get multiCASECPU
-# multiCASECPU('PROP_LEAP')
+# multiCASECPU()
 
 # tun this to get all explicit templates
 # for prop in [k for k in PR.keys()]:
-#     multicase(prop)
+# multicase(prop)
