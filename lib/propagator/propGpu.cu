30289475fdf9a7b15a6c98b0aa4049adf07a1670
Javier Redondo <javier.redondo.martin@gmail.com>
2021-07-02 13:33:45 +0200

    16 bugs

--- lib/propagator/propGpu.cu
+++ lib/propagator/propGpu.cu
@@ -217,11 +217,9 @@ void	propagateGpu(const void * __restrict__ m, void * __restrict__ v, void * __r
 		float  food2[NN] ;
 		for (int i =0; i<NN; i++){
 			food2[i] = (float) (ppar.PC)[i]*ppar.ood2a;
-			LogOut("food %d %f (%f)\n",i,food2[i],ppar.ood2a);
 		}
 
 		const float *ood2 = &(food2[0]);
-		LogOut("ood2 %f (%f)\n",ood2[0],ood2[1]);
 
 		switch (VQcd) {
 
