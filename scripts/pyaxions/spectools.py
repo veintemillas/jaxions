aa68d5b63b215bbd5178503eeec2127c65fc34fd
ksaikawa <saikawa@mpp.mpg.de>
2021-09-16 12:03:57 +0900

    skip the evaluation of esp when the correction matrix becomes singular

--- scripts/pyaxions/spectools.py
+++ scripts/pyaxions/spectools.py
@@ -2116,7 +2116,11 @@ class espevol:
                     m = pa.gm(f,'mspM_'+spmasklabel[5:])
                 elif spmasklabel[0:5] == 'espCK':
                     m = pa.gm(f,'mspM_'+spmasklabel[6:])
-                s1 = (self.sizeL**3)*np.dot(inv(m),s0/self.nm)
+                try:
+                    s1 = (self.sizeL**3)*np.dot(inv(m),s0/self.nm)
+                except:
+                    # skip the evaluation of s1 if m becomes singular
+                    s1 = [np.nan]*len(s0)
                 self.espc.append(s1)
             print('\rbuilt up to log = %.2f [%d/%d]'%(logi,mfnsp.index(f)+1,len(mfnsp)),end="")
         print("")
