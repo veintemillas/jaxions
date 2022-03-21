7496a18e8a7194babd75aed18c284eff65f0bfd3
Javier Redondo <javier.redondo.martin@gmail.com>
2022-03-21 10:26:29 +0100

    estimate of phi implemente

--- test/Paxions.cpp
+++ test/Paxions.cpp
@@ -18,9 +18,12 @@
 #include "meas/measa.h"
 
 #include "WKB/WKB.h"
+#include "gravity/potential.h"
 
 using namespace std;
 using namespace AxionWKB;
+
+double find_saturation_ct(Scalar *axion);
 
 int	main (int argc, char *argv[])
 {
@@ -136,6 +139,14 @@ int	main (int argc, char *argv[])
 	index++;
 	tunePropagator(axion);
 
+
+	/* Saturation */
+	double ct_sat = 1e300;
+	if (axion->BckGnd()->ICData().grav > 0)
+		ct_sat = find_saturation_ct(axion);
+	/*typical value of phi*/
+	double typ_phi = 1;
+
 	if (axion->BckGnd()->ICData().grav>0.0){
 		initGravity(axion);
 		LogOut ("Switch on gravity! (grav %..2e)\n",axion->BckGnd()->ICData().grav);
@@ -180,10 +191,18 @@ int	main (int argc, char *argv[])
 			R/R1 = ct +  0.25 8.55437*10^-11 ct**2
 			*/
 
+			/* Calculate gravitational time scale */
+			calculateGraviPotential();
+			Binner<3000,float> contBin(static_cast<float *>(axion->m2Start()), axion->Size(),
+							[] (float x) -> float { return (double) ( x ) ;});
+			contBin.find();
+			LogOut("max %.3f min %.3f\n\n",contBin.max(),contBin.min());
 
+			typ_phi = max(contBin.max(),-contBin.min());
 
 	}
 	bool sat = false;
+
 
 	LogOut ("Start redshift loop\n\n");
 	for (int iz = 0; iz < nSteps; iz++)
@@ -197,12 +216,18 @@ int	main (int argc, char *argv[])
 		dct < 1/gravi mA
 		*/
 		{
-			double dzg = 1./axion->BckGnd()->ICData().grav/axion->AxionMass();
+			double dzg = 1./axion->BckGnd()->ICData().grav/axion->AxionMass()/typ_phi;
 			if (dzaux > dzg)
 				dzaux = dzg;
 		}
 
-		if (!sat && axion->AxionMass()*(*axion->RV()) > 3.141597/axion->Delta()/axion->BckGnd()->ICData().grav)
+		/*sat the sat*/
+		if (*axion->zV() < ct_sat && *axion->zV() + dzaux > ct_sat)
+			dzaux = ct_sat - *axion->zV();
+
+		/* decrease saturation time to improve */
+		if (!sat && *axion->zV() > ct_sat)
+		// if (!sat && axion->AxionMass()*(*axion->RV()) > 1/axion->Delta()/axion->BckGnd()->ICData().grav)
 		{
 			/*change definition of mA, nqcd */
 			axion->BckGnd()->SetZRestore(*axion->RV());
@@ -260,4 +285,73 @@ int	main (int argc, char *argv[])
 	endAxions();
 
 	return 0;
+}
+
+
+/* Finds the time at which the gravitational term becomes large
+  phase difference between two points
+	int dct mAc [(phi)2-(phi)1] ~ pi
+	or ...
+	int dct mAc grad (phi) delta ~ pi
+	using grad phi ~ k0 phi
+
+	int dct mAc phi /k0 delta ~ pi
+	~ n/2 ct mAc gravi delta * k0 ~ pi
+	I will use k0 the base grid (can be much worse)
+	ct mAc gravi = 1
+*/
+
+
+double find_saturation_ct(Scalar *axion)
+{
+
+	double fff = axion->BckGnd()->Frw();
+
+	if (axion->BckGnd()->Indi3() > 0.0 && (fff > 0.0)){
+	double ct = *axion->zV();
+	double fun0 = axion->AxionMass(ct)*ct*axion->BckGnd()->ICData().grav;
+	double fun = fun0;
+	double ct2 = ct*1.1;
+	double fun2 = axion->AxionMass(ct2)*ct2*axion->BckGnd()->ICData().grav;
+	double k = 1;
+	double a = 1;
+	double meas = std::abs(fun2 - 1);;
+	LogMsg(VERB_NORMAL,"[VAX find_saturation_ct] frw %f indi3 %f ct %e ", fff, axion->BckGnd()->Indi3(), ct );LogFlush();
+	while (meas > 0.001)
+	{
+		/* Assume power law
+		fun ~ k ct^a
+		then fun2/fun = (ct2/ct)^a
+		a = log(fun2/fun)/log(ct2/ct)
+		k = fun2/ct2^a
+		fun = 1 -> ct3 = 1/k^{1/a}, ct2 = ct
+		*/
+		a = std::log(fun2/fun)/std::log(ct2/ct);
+		k = fun2/std::pow(ct2,a);
+		// LogOut("ct %e DWfun %e ct2 %e DWfun2 %e meas %e k %e a %e\n", ct, DWfun, ct2, DWfun2, meas, k ,a );
+		if ((a == 0) && (fun2 > fun0) ){
+			LogMsg(VERB_PARANOID,"[VAX find_saturation_ct] flat slope between ct %e ct2 %e", ct, ct2 );
+			if (fun2 > 1) {
+				LogMsg(VERB_PARANOID,"[VAX findzdoom] Jump back!");
+				ct  = ct2;
+				ct2 = std::sqrt(*axion->zV()*ct2);
+			}
+			else {
+				LogMsg(VERB_PARANOID,"[VAX find_saturation_ct] DWfun will never reach 1");
+				return INFINITY;
+			}
+		} else {
+			ct  = ct2;
+			ct2 = std::pow(k,-1./a);
+		}
+		fun = fun2;
+		fun2 = axion->AxionMass(ct2)*ct2*axion->BckGnd()->ICData().grav;
+		meas = std::abs(fun2 - 1);
+		LogMsg(VERB_PARANOID,"ct2 %e fun2 %e meas %e k %e a %e", ct2, fun2, meas, k ,a );
+	}
+	//LogOut("ct2 %e DWfun2 %e meas %e k %e a %e\n", ct2, DWfun2, meas, k ,a );
+	LogMsg(VERB_NORMAL,"[VAX find_saturation_ct] Saturation time %f ", ct2 );LogFlush();
+	return ct2 ;
+} else {
+	return -1 ; }
 }
