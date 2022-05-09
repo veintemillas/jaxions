b2ac666bdc784af6641b90d1a8992fe6de9d3518
gpierobon <gpierobon@users.noreply.github.com>
2022-05-10 09:25:16 +1000

    General Paxion updates

--- test/Paxions.cpp
+++ test/Paxions.cpp
@@ -23,7 +23,7 @@
 using namespace std;
 using namespace AxionWKB;
 
-double find_saturation_ct(Scalar *axion, double zf);
+double find_saturation_ct (Scalar *axion, FILE *file);
 
 int	main (int argc, char *argv[])
 {
@@ -91,8 +91,10 @@ int	main (int argc, char *argv[])
 	LogOut("zGrid  =  %ld\n",   zGrid);
 	LogOut("z      =  %2.2f\n", z_now);
 	LogOut("zthr   =  %3.3f\n", myCosmos.ZThRes());
-	LogOut("zres   =  %3.3f\n", myCosmos.ZRestore());
-	LogOut("mass   =  %3.3f\n\n", axion->AxionMass());
+	LogOut("zres   =  %.2e\n", myCosmos.ZRestore());
+	LogOut("mass   =  %3.3f\n", axion->AxionMass());
+	LogOut("grav   =  %.1e\n",axion->BckGnd()->ICData().grav);
+	LogOut("beta   =  %.1f\n\n",axion->BckGnd()->ICData().beta);
 
 	if (axion->Precision() == FIELD_SINGLE)
 		LogOut("precis = SINGLE(%d)\n",FIELD_SINGLE);
@@ -101,9 +103,6 @@ int	main (int argc, char *argv[])
 
 	LogOut("--------------------------------------------------\n");
 
-	//--------------------------------------------------
-	//       MEASUREMENT
-	//--------------------------------------------------
 	//- Measurement
 	MeasData lm;
 	//- number of plaquetes pierced by strings
@@ -139,6 +138,13 @@ int	main (int argc, char *argv[])
 	index++;
 	tunePropagator(axion);
 
+	FILE *file_sat;
+	file_sat = NULL;
+	char base[32];
+	sprintf(base, "pout/info_saturation.txt");
+	file_sat = fopen(base,"w+");
+	fprintf(file_sat, "# Time, Grad max, Grad mean, Saturation time\n\n");
+
 
 	/* Saturation */
 	double ct_sat = 1e300;
@@ -146,17 +152,17 @@ int	main (int argc, char *argv[])
 	/*typical value of phi*/
 	double typ_phi = 1;
 
-	if (axion->BckGnd()->ICData().grav>0.0){
+	if (axion->BckGnd()->ICData().grav>0.0)
+	{
 
-		LogOut ("Switch on gravity! (grav %..2e)\n",axion->BckGnd()->ICData().grav);
+		LogOut ("Switch on gravity! (grav %.2e)\n",axion->BckGnd()->ICData().grav);
 		initGravity(axion);
 
-		if (axion->BckGnd()->ICData().grav_hyb){
+		if (axion->BckGnd()->ICData().grav_hyb)
+		{
 			LogOut ("Tunning hybrid method\n");
 			tuneGravityHybrid	();
-
 		}
-
 
 		/* Conversion of Units ?
 			we could convert
@@ -207,19 +213,19 @@ int	main (int argc, char *argv[])
 			typ_phi = max(contBin.max(),-contBin.min());
 
 			/* Assumes gravitational field in m2start */
-			ct_sat = find_saturation_ct(axion,zFinl);
-			LogOut("Saturation time %.2f\n",ct_sat);
+			ct_sat = find_saturation_ct(axion,file_sat);
+			LogOut("First Saturation time %.2f\n\n",ct_sat);
 
 	}
+	
 	bool sat = false;
-
 
 	LogOut ("Start redshift loop\n\n");
 	for (int iz = 0; iz < nSteps; iz++)
 	{
 
 		dzaux = (uwDz) ? axion->dzSize() : (zFinl-zInit)/nSteps ;
-
+		
 		/* normalise dynamical graavity time-step?
 		Option 1, (Naive) allow only phase~1 per iteration in the point with the largest grav-pot.
 		there is really not need because our integrator is exact in V as we alternate V and K Kick operators. */
@@ -241,73 +247,94 @@ int	main (int argc, char *argv[])
 		if (*axion->zV() < axion->BckGnd()->ZThRes() && *axion->zV() + dzaux > axion->BckGnd()->ZThRes())
 			dzaux = axion->BckGnd()->ZThRes() - *axion->zV();
 
+		if (!(iz%dump)){
+			measrightnow = true;
+		}
 
-		/* decrease saturation time to improve */
+		propagate (axion, dzaux);
+		//ct_sat = find_saturation_ct(axion, file_sat);
+		
 		if (axion->BckGnd()->ICData().grav_sat)
-			if (!sat && *axion->zV() == ct_sat)
-			{
+		{
+			if (*axion->zV() >= ct_sat && !sat)
+			{			
 				/* We want to saturate the conformal axion mass to be constant from this moment on
-				this means that mA R = (mA R)_now
-				in power-law cosmology R=ct^frw
-				mA^2 = 1/R^2frw, which we can achieve with nqcd = -2frw
-				nqcd is only active below zthreshold or above zrestore
-				if ct<Rc, then set Rc=R_restore=Rnow
-				if ct>Rc, set R_restore=R_now
-				if ct>Rrestore>Rc, need to change indi3 (because of the way Rthres is implemented)
-				 */
-				/*change definition of mA, nqcd */
-				if (ninfa.printconf & PRINTCONF_PAXIONSAT)
-					writeConf(axion,index); 
-				
+		 		this means that mA R = (mA R)_now
+		 		in power-law cosmology R=ct^frw
+		 		mA^2 = 1/R^2frw, which we can achieve with nqcd = -2frw
+		 		nqcd is only active below zthreshold or above zrestore
+		 		if ct<Rc, then set Rc=R_restore=Rnow
+		 		if ct>Rc, set R_restore=R_now
+		 		if ct>Rrestore>Rc, need to change indi3 (because of the way Rthres is implemented)
+		 		 */
 				double R0 = *axion->RV();
 				double Rc = axion->BckGnd()->ZThRes();
 				double Rr = axion->BckGnd()->ZRestore();
 				double aa = 2*axion->BckGnd()->Frw();
-				if (R0 <= Rc){
+				if (R0 <= Rc)
+				{
 					axion->BckGnd()->SetZThRes(R0);
 					axion->BckGnd()->SetZRestore(R0);
 					axion->BckGnd()->SetQcdExpr(-aa);
-					LogOut("Linear gravitaty resolution limit achived! SET Rc=Rr = %e and n = %f\n",R0,-aa);
-				} else if (R0 > Rc && R0 <= Rr) {
+					LogOut("--------------------------------------------------------------------------------------------------------");
+					LogOut("\nSaturation time %e",*axion->zV());
+					LogOut("\nLinear gravitational resolution limit achieved! Setting Rc=Rr = %e and n = %.1f\n",R0,-aa);
+				} 
+				else if (R0 > Rc && R0 <= Rr) 
+				{
 					axion->BckGnd()->SetZRestore(R0);
 					axion->BckGnd()->SetQcdExpr(-aa);
-					LogOut("Linear gravitaty resolution limit achived! SET Rr = %e (Rc = %e) and n = %f\n",R0,Rc,-aa);
-				} else if (R0 > Rc && R0 > Rr) {
+					LogOut("--------------------------------------------------------------------------------------------------------");
+					LogOut("\nSaturation time %e",*axion->zV());
+					LogOut("\nLinear gravitational resolution limit achieved! Setting Rr = %e (Rc = %e) and n = %.1f\n",R0,Rc,-aa);
+				} 
+				else if (R0 > Rc && R0 > Rr) 
+				{
 					// we will keep Rc, Rr, but will change indi3 to have the same conformal mass with the new nqcd = -2*frw
 					// aMassSq = indi3**2*(Rc)**nqcd * (R/Rr)**nqcd = indi3**2*(Rc R / Rr)**nqcd
 					// indi3_neq = aMass(nqcd)/[(Rc R / Rr)]**(-frw)
 					double newindi3 = axion->AxionMass()/pow(Rc*R0/Rr,-aa);
 					axion->BckGnd()->SetIndi3(-aa);
 					axion->BckGnd()->SetQcdExpr(-aa);
-					LogOut("Linear gravitaty resolution limit achived! RESET indi3 = %e (Rc = %e, Rr = %e) and n = %f\n",newindi3,Rc,Rr,-aa);
+					LogOut("--------------------------------------------------------------------------------------------------------");
+					LogOut("\nSaturation time %e",*axion->zV());
+					LogOut("\nLinear gravitational resolution limit achieved! Resetting indi3 = %e (Rc = %e, Rr = %e) and n = %.1f\n",newindi3,Rc,Rr,-aa);
 				}
 				// need to renormalise also indi3
-				// have to make aMass = indi3*indi3*pow(zThRes, nQcd)_old= indi3*indi3*pow(zThRes, -1)
-
+		 		// have to make aMass = indi3*indi3*pow(zThRes, nQcd)_old= indi3*indi3*pow(zThRes, -1)
+				
+				if (ninfa.printconf & PRINTCONF_PAXIONSAT)
+				{
+					LogOut("Dumping configuration %05d for Gadget ... ",index);
+					writeConf(axion,index);
+					LogOut("done! \n");
+					LogOut("--------------------------------------------------------------------------------------------------------\n");
+					
+				}
+				else
+					LogOut("--------------------------------------------------------------------------------------------------------\n");
 				sat = true;
 			}
-
-
-		if (!(iz%dump)){
-			measrightnow = true;
 		}
 
-		propagate (axion, dzaux);
 		counter++;
 
-		// Break the loop when we are done
-		if ( (*axion->zV()) >= zFinl ){
+		if ( (*axion->zV()) >= zFinl )
+		{
+			LogOut("--------------------------------------------------------------------------------------------------------\n");
 			LogOut("zf reached! ENDING ... \n"); fflush(stdout);
 			break;
 		}
-		if ( abs((*axion->zV())-zFinl) < 1.0e-10 ){
+		if ( abs((*axion->zV())-zFinl) < 1.0e-10 )
+		{
+			LogOut("--------------------------------------------------------------------------------------------------------\n");
 			LogOut("zf approximately reached! ENDING ... \n"); fflush(stdout);
 			break;
 		}
 
-		// Partial analysis
-		if(measrightnow){
-
+		if(measrightnow)
+		{
+			ct_sat = find_saturation_ct(axion, file_sat);
 			ninfa.index=index;
 			lm = Measureme (axion, ninfa);
 			index++;
@@ -316,18 +343,19 @@ int	main (int argc, char *argv[])
 		}
 	}
 
-	//--------------------------------------------------
-	//       SAVE DATA
-	//--------------------------------------------------
-
 	ninfa.index++;
-
-	LogOut ("\n\n Dumping configuration %05d ...", ninfa.index);
-	writeConf(axion, ninfa.index);
-	LogOut ("Done!\n\n");
-
-
-	LogOut ("Printing FINAL measurement file %05d ... ", ninfa.index);
+	
+	fclose(file_sat);
+	
+	if (ninfa.printconf & PRINTCONF_FINAL)
+	{
+		LogOut ("Dumping configuration %05d ...", ninfa.index);
+		writeConf(axion, ninfa.index);
+		LogOut ("done!\n");
+	}
+	
+	LogOut ("Printing FINAL measurement file %05d \n", ninfa.index);
+	LogOut("--------------------------------------------------------------------------------------------------------\n");
 
 	Measureme (axion, ninfa);
 
@@ -336,6 +364,10 @@ int	main (int argc, char *argv[])
 	return 0;
 }
 
+
+/*  
+	AUXILIARY FUNCTIONS
+*/  
 
 /* Finds the time at which the gravitational term becomes large
   phase difference between two points
@@ -346,11 +378,8 @@ int	main (int argc, char *argv[])
 	I will calculate
 	ct mA gravi phi12_code = 1
 */
-
-
-double find_saturation_ct(Scalar *axion, double zFinl)
+double find_saturation_ct(Scalar *axion, FILE *file)
 {
-
 	double fff = axion->BckGnd()->Frw();
 
 	if (axion->BckGnd()->Indi3() > 0.0 && (fff > 0.0)){
@@ -381,7 +410,7 @@ double find_saturation_ct(Scalar *axion, double zFinl)
 	}
 	grad_mean /= axion->Size()-axion->Surf();
 	LogMsg(VERB_NORMAL,"[VAX find_saturation_ct] grad max %.2e grad_mean %.2e",grad_max, grad_mean);
-	LogOut("[VAX find_saturation_ct] grad max %.2e grad_mean %.2e\n",grad_max, grad_mean);
+	//LogOut("[VAX find_saturation_ct] grad max %.2e grad_mean %.2e\n",grad_max, grad_mean);
 
 	double ct = *axion->zV();
 	double phi12 = axion->BckGnd()->ICData().grav*grad_max;
@@ -432,6 +461,7 @@ double find_saturation_ct(Scalar *axion, double zFinl)
 		LogMsg(VERB_NORMAL,"ct2 %e fun2 %e meas %e k %e a %e", ct2, fun2, meas, k ,a );
 	}
 	//LogOut("ct2 %e DWfun2 %e meas %e k %e a %e\n", ct2, DWfun2, meas, k ,a );
+	fprintf(file, "%e %f %f %e\n",*axion->zV(),grad_max,grad_mean,ct2);
 	LogMsg(VERB_NORMAL,"[VAX find_saturation_ct] Saturation time %f ", ct2 );LogFlush();
 	return ct2 ;
 } else {
