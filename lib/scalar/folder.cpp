4a0481e25d3aaeafece84b9985a0b3a658a060a6
veintemillas <jredondo@unizar.es>
2025-06-28 08:29:24 +0200

    fo 1

--- lib/scalar/folder.cpp
+++ lib/scalar/folder.cpp
@@ -115,7 +115,7 @@ void	Folder::unfoldField2D (const size_t sZ)
 
 	if (!field->Folded())
 	{
-		LogMsg (VERB_HIGH, "unfoldField2D called in an unfolded configuration, copying data to ghost zones");LogFlush();
+		LogMsg (VERB_HIGH, "unfoldField2D called in an unfolded configuration, copying %d slice to ghost zone 1",sZ);LogFlush();
 		memcpy (mg1, &m[n2*sZ], sizeof(cFloat)*n2);
 		memcpy (mg2, &v[n2*sZ], sizeof(cFloat)*n2);
 		return;
@@ -744,7 +744,7 @@ void	Folder::operator()(FoldType fType, size_t cZ)
 					default:
 						break;
 				}
-				
+
 				break;
 
 		default:
