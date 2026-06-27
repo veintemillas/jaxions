57cb785a23fba8a83ddaddca79e808044e954dcc
veintemillas <jredondo@unizar.es>
2026-06-27 12:10:31 +0200

    external B-field to create angular momentum in collapse

--- lib/gen/anystringXeon.cpp
+++ lib/gen/anystringXeon.cpp
@@ -34,7 +34,7 @@ void anystringXeon (Scalar *field, IcData ic,
     size_t local_z_start = size_t(rank) * Lz;
 
     LogMsg(VERB_NORMAL,"[θX] Calculation with %d copies in dipole approx",ic.kMax);
-    cthetaSolidAngleXeon(field->mStart(), Lx, Lz, local_z_start, Lz*nSplit, field->Precision(), len, xs, ys, zs, eps,ic.kMax);
+    cthetaSolidAngleXeon(field->mStart(), Lx, Lz, local_z_start, Lz*nSplit, field->Precision(), len, xs, ys, zs, eps,ic.kMax, ic.Bext, ic.kcr);
 
 }
 
